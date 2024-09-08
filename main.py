f11cd47ea9fb90ca1bab08de8184c9d2977ee9ec
Alex Belanger <nitrix@nitrix.me>
2024-09-08 16:55:39 -0400

    Getting even more wood!

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 while True:
-	if can_harvest():
-		harvest()
-		plant(Entities.Bush)
-	move(South)
+	for y in range(3):
+		for x in range(3):
+			if can_harvest():
+				harvest()
+				plant(Entities.Bush)
+			move(East)
+		move(South)
