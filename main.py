b05376193be0881275be60104bd4c10b95c139b7
Alex Belanger <nitrix@nitrix.me>
2024-09-08 17:35:35 -0400

    Hay, Trees and Carrots.

--- main.py
+++ main.py
@@ -1,19 +1,45 @@
-while True:
-	# Buy carrot seeds if we don't have any.
-	if num_items(Items.Carrot_Seed) < 3:
-		for i in range(3-num_items(Items.Carrot_Seed)):
-			trade(Items.Carrot_Seed)
+def restock(item, qty):
+	if num_items(item) < qty:
+		needed = qty - num_items(item)
+		for i in range(needed):
+			trade(item)
 
-	# Plant and harvest wood and carrots.
-	for y in range(3):
-		for x in range(3):
-			if can_harvest():
-				harvest()
-			if get_pos_y() == 1:
-				plant(Entities.Bush)
-			if get_pos_y() == 2:
-				if get_ground_type() == Grounds.Turf:
-					till()
-				plant(Entities.Carrots)
-			move(East)
-		move(South)
+def processTile(desiredGroundType, desiredEntity):
+	# Desired ground type.
+	currentGroundType = get_ground_type()
+	if currentGroundType != desiredGroundType:
+		if currentGroundType == Grounds.Turf and desiredGroundType == Grounds.Soil:
+			till()
+		if currentGroundType == Grounds.Soil and desiredGroundType == Grounds.Turf:
+			till()
+
+	# Desired entity - harvesting first.
+	if can_harvest():
+		harvest()
+
+	# Desired entity - planting second.
+	plant(desiredEntity)
+
+# Chess-like grid pattern for planting (true/false instead of white/black).
+def chessLikePattern():
+	return (get_pos_y() + get_pos_x()) % 2
+
+def main():
+	while True:
+		# Buy carrot seeds if we don't have any.
+		restock(Items.Carrot_Seed, 4)
+
+		# Plant and harvest wood and carrots.
+		for y in range(4):
+			for x in range(4):
+				if get_pos_y() == 3:
+					processTile(Grounds.Soil, Entities.Carrots)
+				else:
+					if chessLikePattern() == 0:
+						processTile(Grounds.Turf, Entities.Tree)
+					else:
+						processTile(Grounds.Turf, Entities.Grass)
+				move(East)
+			move(South)
+
+main()
